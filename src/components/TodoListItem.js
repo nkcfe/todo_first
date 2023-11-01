ed3e9e3ff0fd5e7602e1bc495d51fd457dbd9bb4
nkc930604@gmail.com <nkc930604@gmail.com>
2023-11-01 21:45:27 +0900

    first commit

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  MdCheckBox,
+  MdCheckBoxOutlineBlank,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss';
+
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  // 구조분해할당으로 받아옴.
+  const { id, text, checked } = todo;
+
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
